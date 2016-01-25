bb770d2e4edef9ffe8fbbb2cec0265b6c81c1255
Deep Sohelia <soheliadeep@gmail.com>
2016-01-25 12:15:46 +0000

    Update script.js

--- deep/script.js
+++ deep/script.js
@@ -4,7 +4,7 @@ window.onload = function() {
         console.log(evt.target.innerText);
         console.log(evt.target.tagName)
         console.log(evt.target.parentNode.id)
-        if((evt.target.tagName == "A") && (evt.target.innerText == "Read more")) {
+        if((evt.target.tagName == "A") && (evt.target.innerText == "Read More")) {
             evt.preventDefault();
             evt.target.parentNode.className = "fs";
             evt.target.innerText = "Go back"
