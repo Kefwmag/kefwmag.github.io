989f1a8c481a8bcb7f402c07f37a5d4453cd1797
Deep Sohelia <soheliadeep@gmail.com>
2016-01-25 11:12:17 +0000

    Create script.js

---
+++ deep/script.js
@@ -0,0 +1,19 @@
+window.onload = function() {
+    document.addEventListener('click', function(evt) {
+        console.log(evt.target.parentNode);
+        console.log(evt.target.innerText);
+        console.log(evt.target.tagName)
+        console.log(evt.target.parentNode.id)
+        if((evt.target.tagName == "A") && (evt.target.innerText == "Read more")) {
+            evt.preventDefault();
+            evt.target.parentNode.className = "fs";
+            evt.target.innerText = "Go back"
+            history.pushState({}, evt.target.parentNode.id + ".html", window.location.href + "/?" + evt.target.parentNode.id);
+        } else if((evt.target.tagName == "A") && (evt.target.innerText == "Go back")) {
+            history.pushState({}, "Home", "http://kefwmag.github.io/deep");
+            evt.preventDefault();
+            evt.target.parentNode.className = "";
+            evt.target.innerText = "Read more";
+        }
+    }, false);
+}
