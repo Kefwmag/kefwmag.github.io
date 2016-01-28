7a962068187ab92ef572864afbb3eb528d8e7708
Deep Sohelia <soheliadeep@gmail.com>
2016-01-28 09:41:41 +0000

    Update script.js

--- deep/script.js
+++ deep/script.js
@@ -16,10 +16,11 @@ window.onload = function() {
     var cat = catHead.innerHTML;
     if (cat == "news ") {
         catHead.innerHTML = "School News";
-        document.getElementsByTagName("main")[0].innerHTML += "<style>header { background-color:#d90000;border-bottom: 3px solid #9A1504;}section {    border-bottom: 3px solid #9A1504;color: #d90000;}main div article a {color:#d90000;}</style>"
+        document.getElementsByTagName("main")[0].innerHTML += "<style>header { background-color:#d90000;border-bottom: 3px solid #9A1504;}section {    border-bottom: 3px solid #9A1504;color: #d90000;}main div article a {color:#d90000;}</style>";
     }
     if (cat == "tech ") {
         catHead.innerHTML = "Technology";
+        document.getElementsByTagName("main")[0].innerHTML += "<style>header { background-color:#24bba8;border-bottom: 3px solid #088A79;}section {    border-bottom: 3px solid #088a79;color: #d90000;}main div article a {color:#24bba8;}</style>"
     }
     if (cat == "arts ") {
         catHead.innerHTML = "Arts & Entertainment";
